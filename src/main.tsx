df1793cc0652f82d47d80202401f8cedafab717c
Sachin-kalaiselvan <161479509+Sachin-kalaiselvan@users.noreply.github.com>
2026-08-04 02:40:35 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
+import { LangProvider } from "./i18n";
 import App from "./App";
 import "./index.css";
 
@@ -10,7 +11,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <LangProvider>
+          <App />
+        </LangProvider>
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
